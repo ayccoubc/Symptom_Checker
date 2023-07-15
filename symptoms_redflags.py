bf471d86e3717e24161e6853b525c732d14ed03f
ayccoubc <129595561+ayccoubc@users.noreply.github.com>
2023-07-15 16:22:19 +0530

    Add files via upload

---
+++ symptoms_redflags.py
@@ -0,0 +1,26 @@
+symptoms = {
+    'Back Pain': {
+        'Symptoms': ['Dull or throbbing back pain', 'Stiffness', 'Muscle spasms', 'Limited range of motion', 'Numbness or tingling sensation', 'Difficulty standing or walking'],
+        'Red Flags': ['Severe or progressive weakness or numbness in the legs', 'Loss of bladder or bowel control', 'Difficulty walking or standing', 'Unexplained weight loss', 'History of cancer, recent trauma, or prolonged steroid use', 'Fever, chills, or night sweats associated with back pain']
+    },
+    'Headache': {
+        'Symptoms': ['Dull or throbbing head pain', 'Sensitivity to light', 'Sensitivity to sound', 'Nausea and vomiting', 'Fatigue or tiredness', 'Difficulty concentrating'],
+        'Red Flags': ['Sudden, severe headache ("thunderclap headache") that develops rapidly', 'Headache following head trauma or injury', 'New-onset headache in individuals over 50 years of age', 'Changes in vision, speech, or coordination along with the headache', 'Headache that progressively worsens and does not respond to over-the-counter pain relievers', 'History of cancer or HIV/AIDS']
+    },
+    'Sinusitis': {
+        'Symptoms': ['Facial pain or pressure', 'Nasal congestion', 'Thick nasal discharge', 'Headache', 'Cough', 'Fatigue'],
+        'Red Flags': ['Severe headache or facial pain that worsens over time', 'Vision changes or double vision', 'Stiff neck or difficulty bending the neck forward', 'Severe swelling or redness around the eyes or forehead', 'Changes in mental status, confusion, or altered consciousness', 'Symptoms that persist for more than 10 days or worsen after initial improvement']
+    },
+    'Stomachache': {
+        'Symptoms': ['Aching or cramping sensation in the stomach', 'Nausea and vomiting', 'Bloating or gas', 'Loss of appetite', 'Indigestion or heartburn', 'Diarrhea or constipation'],
+        'Red Flags': ['Severe or persistent abdominal pain that is unrelieved by rest or position changes', 'Abdominal pain accompanied by vomiting blood or passing dark, tarry stools', 'Severe bloating or swelling of the abdomen', 'Signs of internal bleeding, such as dizziness, fainting, or rapid heartbeat', 'Yellowing of the skin or eyes (jaundice)', 'Recent abdominal surgery or injury']
+    },
+    'Gastroenteritis (Stomach flu)': {
+        'Symptoms': ['Nausea', 'Vomiting', 'Diarrhea', 'Abdominal cramps', 'Loss of appetite', 'Fever'],
+        'Red Flags': ['Severe abdominal pain or cramping', 'Persistent vomiting and inability to keep fluids down', 'Signs of dehydration, such as excessive thirst, dry mouth, or decreased urine output', 'Blood in the stool or vomit', 'High fever (typically above 101°F or 38.3°C)', 'Symptoms that persist for more than a few days or worsen over time']
+    },
+    'Urinary tract infection (UTI)': {
+        'Symptoms': ['Frequent urge to urinate', 'Pain or burning sensation during urination', 'Cloudy or bloody urine', 'Lower abdominal pain', 'Strong-smelling urine', 'Fever or back pain'],
+        'Red Flags': ['Severe or worsening abdominal or pelvic pain', 'Blood in the urine (hematuria)', 'High fever (typically above 101°F or 38.3°C)', 'Persistent vomiting or inability to keep fluids down', 'Symptoms that do not improve within 48 hours despite home remedies', 'History of kidney problems or UTIs in men or pregnant women']
+    }
+}
